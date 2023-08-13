55af8d03cd939c30b9d29975a7c776a922bee012
Anastasiya <v.anastasiya26@mail.ru>
2023-08-13 23:06:55 +0700

    uploader-app

--- project/libs/models/blog-models/prisma/migrations-copy/20230810163424_add_cascade_delete_update_comment/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "comments" DROP CONSTRAINT "comments_postPostId_fkey";
-
--- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "comments_postPostId_fkey" FOREIGN KEY ("postPostId") REFERENCES "posts"("post_id") ON DELETE CASCADE ON UPDATE CASCADE;
