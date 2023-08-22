3e5b061ee09c1f38821ec91d9a75bf5738132c3c
Anastasiya <v.anastasiya26@mail.ru>
2023-08-22 23:10:32 +0700

    task 5.5

--- project/libs/shared/app-types/src/index.ts
+++ project/libs/shared/app-types/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/type.interface';
 export * from './lib/comment.interface';
 export * from './lib/post.interface';
 export * from './lib/file.interface';
+export * from './lib/token-payload.interface';
